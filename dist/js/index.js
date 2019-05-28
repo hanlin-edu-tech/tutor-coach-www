0d625aa9cac20f90e862062ebba72c69755ce6e2
UJHung <ujhong636@gmail.com>
2019-05-28 10:28:19 +0800

    新增widget

--- dist/js/index.js
+++ dist/js/index.js
@@ -33,3 +33,15 @@ $(document).click(function(event) {
         $("body").find(".modal").removeClass("visible");
     }
 });
+
+
+// e導師close
+$(".close-ecoach").click(function(){
+    $(".ecoach").addClass("hide-coach");
+    $(".ecoach-icon").addClass("show-icon");
+});
+
+$(".ecoach-icon").click(function(){
+    $(".ecoach").removeClass("hide-coach");
+    $(".ecoach-icon").removeClass("show-icon");
+});
