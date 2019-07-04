583636f0aef41507e4b167375f27f9cb33c2b220
infinityAlive <sonicx7boyboyboy@gmail.com>
2019-07-05 07:29:23 +0800

    增加素材類別

--- src/js/modules/util/modal-text.js
+++ src/js/modules/util/modal-text.js
@@ -3,7 +3,7 @@ const AuthText = {
 }
 
 const PopupText = {
-  reward (coins, gems, message) {
+  reward (coins, gems, message = '') {
     return `恭喜獲得金幣 ${coins} 寶石 ${gems}<br/>${message}`
   },
   REWARD_ERROR: '獎勵領取失敗',
@@ -13,12 +13,13 @@ const ItemType = new Map()
 ItemType.set('NU_EHANLIN-VIDEO', '教學影片')
 ItemType.set('NU_EHANLIN-UNIT', '單元評量')
 ItemType.set('NU_EHANLIN-QUIZ', '隨堂測驗')
+ItemType.set('NU_EHANLIN-FIXED', '隨堂測驗')
 
 const ItemIconClass = new Map()
-ItemIconClass.set('NU_EHANLIN-VIDEO', 'flaticon-widget-masterlist_video')
-ItemIconClass.set('NU_EHANLIN-UNIT', 'flaticon-widget-masterlist_experiment')
-ItemIconClass.set('NU_EHANLIN-FIXED', 'flaticon-widget-masterlist_experiment')
-ItemIconClass.set('NU_EHANLIN-QUIZ', 'flaticon-widget-masterlist_classquiz')
+ItemIconClass.set('NU_EHANLIN-VIDEO', 'flaticon-masterlist_video')
+ItemIconClass.set('NU_EHANLIN-UNIT', 'flaticon-masterlist_experiment')
+ItemIconClass.set('NU_EHANLIN-FIXED', 'flaticon-masterlist_experiment')
+ItemIconClass.set('NU_EHANLIN-QUIZ', 'flaticon-masterlist_classquiz')
 
 export {
   AuthText, PopupText, ItemType, ItemIconClass
