a8c7283a4634234a13afb57c69c613f9bcabe8f8
infinityAlive <sonicx7boyboyboy@gmail.com>
2019-07-12 11:48:21 +0800

    還原設定檔

---
+++ src/js/modules/firestore/firebase-auth.js
@@ -0,0 +1,21 @@
+const testConfig = {
+  apiKey: 'AIzaSyAH5SciA348PxJnwrZWgJEo99rfOvcULS8',
+  authDomain: 'tutor-test-238709.firebaseapp.com',
+  databaseURL: 'https://tutor-test-238709.firebaseio.com',
+  projectId: 'tutor-test-238709',
+  storageBucket: 'tutor-test-238709.appspot.com',
+  messagingSenderId: '184800465453',
+  appId: '1:184800465453:web:9a81935d3a4d57e8'
+}
+
+const productionConfig = {
+  apiKey: 'AIzaSyApascu4ux1iHVA_dD7GDbLyHQUWDay8uw',
+  authDomain: 'coach-245411.firebaseapp.com',
+  databaseURL: 'https://coach-245411.firebaseio.com',
+  projectId: 'coach-245411',
+  storageBucket: 'coach-245411.appspot.com',
+  messagingSenderId: '745967530532',
+  appId: '1:745967530532:web:de55031a35d6d680'
+}
+
+export default testConfig
