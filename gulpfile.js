c3c9ce77a0a101151796b80fe39b661aa5799b2b
AaronChuang <aaron@ehanlin.com.tw>
2020-11-27 09:45:23 +0800

    更新node 版本

--- gulpfile.js
+++ gulpfile.js
@@ -235,5 +235,5 @@ gulp.task('packageToProduction',
 gulp.task('watch', gulp.series('copyToDist', gulp.parallel(watchPugSassImages)))
 
 /* 上傳 GCS */
-gulp.task('uploadToGcsTest', uploadToGCS.bind(uploadToGCS, 'tutor-test-apps/', 'tutor-test-238709', './tutor-test.json', 'no-store'))
-gulp.task('uploadToGcsProduction', uploadToGCS.bind(uploadToGCS, 'tutor-apps/', 'tutor-204108', './tutor.json', 'public, max-age=10800'))
+gulp.task('uploadGcsTest', uploadToGCS.bind(uploadToGCS, 'tutor-test-apps/', 'tutor-test-238709', './tutor-test.json', 'no-store'))
+gulp.task('uploadGcsProd', uploadToGCS.bind(uploadToGCS, 'tutor-apps/', 'tutor-204108', './tutor.json', 'public, max-age=10800'))
