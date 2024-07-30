c0af6a14c5e337897522471da0e1f70168d69c1e
AaronChuang <aaron@ehanlin.com.tw>
2024-07-30 14:00:08 +0800

    更新獎品圖案位置

--- src/js/modules/items.js
+++ src/js/modules/items.js
@@ -29,25 +29,25 @@ export default {
         if(result){
           vueModel.items.push({
             id:"item1",
-            url:"/coach-web/img/item1.png",
+            url: `https://public.ehanlin.com.tw/ehanlin-theme-activity/${result["item1-awardId"]}.png`,
             name: result["item1-name"],
             point: result["item1-point"],
           })
           vueModel.items.push({
             id:"item2",
-            url:"/coach-web/img/item2.png",
+            url: `https://public.ehanlin.com.tw/ehanlin-theme-activity/${result["item2-awardId"]}.png`,
             name: result["item2-name"],
             point: result["item2-point"],
           })
           vueModel.items.push({
             id:"item3",
-            url:"/coach-web/img/item3.png",
+            url: `https://public.ehanlin.com.tw/ehanlin-theme-activity/${result["item3-awardId"]}.png`,
             name: result["item3-name"],
             point: result["item3-point"],
           })
           vueModel.items.push({
             id:"item4",
-            url:"/coach-web/img/item4.png",
+            url: `https://public.ehanlin.com.tw/ehanlin-theme-activity/${result["item4-awardId"]}.png`,
             name: result["item4-name"],
             point: result["item4-point"],
           })
